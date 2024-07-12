6e19b673ae35acc5f6e0625073e477a70afce6a6
JusteKal <66133903+JusteKal@users.noreply.github.com>
2024-07-12 11:26:20 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,41 +3,50 @@ document.addEventListener('DOMContentLoaded', function () {
     var menu = document.querySelector('#menu-icon');
     var navlist = document.querySelector('.navlist');
 
-    // Initial check to see if the header should be hidden
-    if (window.scrollY > 50) {
-        header.classList.add('hidden-nav');
-    }
-
-    // Add event listener for scroll to toggle header visibility
-    window.addEventListener('scroll', function () {
+    if (header) {
+        // Initial check to see if the header should be hidden
         if (window.scrollY > 50) {
             header.classList.add('hidden-nav');
-        } else {
-            header.classList.remove('hidden-nav');
         }
-    });
 
-    // Toggle menu and navlist classes on menu icon click
-    menu.onclick = function () {
-        menu.classList.toggle('bx-x');
-        navlist.classList.toggle('open');
+        // Add event listener for scroll to toggle header visibility
+        window.addEventListener('scroll', function () {
+            if (window.scrollY > 50) {
+                header.classList.add('hidden-nav');
+            } else {
+                header.classList.remove('hidden-nav');
+            }
+        });
     }
 
-    // ScrollReveal configuration
-    const sr = ScrollReveal({
-        distance: '65px',
-        duration: 2600,
-        delay: 450,
-        reset: true
-    });
+    if (menu && navlist) {
+        // Toggle menu and navlist classes on menu icon click
+        menu.addEventListener('click', function () {
+            menu.classList.toggle('bx-x');
+            navlist.classList.toggle('open');
+        });
+    }
 
-    // Reveal elements with ScrollReveal
-    sr.reveal('.tattoo-text', { delay: 200, origin: 'top' });
-    sr.reveal('.tattoo-image', { delay: 450, origin: 'top' });
-    sr.reveal('.icons', { delay: 500, origin: 'right' });
-    sr.reveal('.scroll-down', { delay: 500, origin: 'down' });
+    // Check if ScrollReveal is defined
+    if (typeof ScrollReveal !== 'undefined') {
+        // ScrollReveal configuration
+        const sr = ScrollReveal({
+            distance: '65px',
+            duration: 2600,
+            delay: 450,
+            reset: true
+        });
 
-    sr.reveal('.contact-info', { delay: 300, origin: 'top' });
-    sr.reveal('.map', { delay: 400, origin: 'top' });
-    sr.reveal('.contact-texte', { delay: 200, origin: 'top' });
+        // Reveal elements with ScrollReveal
+        sr.reveal('.tattoo-text', { delay: 200, origin: 'top' });
+        sr.reveal('.tattoo-image', { delay: 450, origin: 'top' });
+        sr.reveal('.icons', { delay: 500, origin: 'right' });
+        sr.reveal('.scroll-down', { delay: 500, origin: 'down' });
+
+        sr.reveal('.contact-info', { delay: 300, origin: 'top' });
+        sr.reveal('.map', { delay: 400, origin: 'top' });
+        sr.reveal('.contact-texte', { delay: 200, origin: 'top' });
+    } else {
+        console.error('ScrollReveal is not defined');
+    }
 });
